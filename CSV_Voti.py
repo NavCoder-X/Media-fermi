e74b1e6b88ec80cc611e33645af9928637b5d9cc
NavCoder-X <nav03504@gmail.com>
2025-08-05 15:04:13 +0200

    Inizzializaione
    Creato il script il 05/08/2025

---
+++ CSV_Voti.py
@@ -0,0 +1,74 @@
+# csv
+
+def csv():
+    import csv
+    import os
+    import platform
+    import subprocess
+    materie_voto={}
+    lista=[]
+    iter=0
+    with open("voti.txt","r") as file:
+        context=file.readlines()
+        for i in context:
+            i=i.strip()
+            if iter==0:
+                lista.append(i)
+                iter=1
+            elif len(str(i))>5:
+                try:
+                    materie_voto[lista[0]]=lista[1:]
+                except:
+                    continue
+                lista=[]
+                lista.append(i)
+            else:
+                lista.append(i)
+
+    with open('example.csv', 'w', newline='') as csvfile:
+        fieldnames = ['materia', 'voto_1','voto_2','voto_3','voto_4','voto_5','voto_6','voto_7','voto_8','voto_9','voto_10', 'media']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for i in materie_voto.keys():
+            voti=materie_voto[i]
+            totale=0
+            for voto in voti:
+                voto = voto.strip()
+                if "-" in voto:
+                    voto=voto.replace("-", "")
+                    voto=float(voto)-0.25
+                elif "+" in voto:
+                    voto=voto.replace("+", "")
+                    voto=float(voto)+0.25
+                elif "½" in voto:
+                    voto=voto.replace("½", "")
+                    voto=float(voto)+0.5
+                else:
+                    voto=float(voto)
+                totale+=voto
+                m=totale/(len(voti))
+            while len(voti)<10:
+                voti.append(0)
+            writer.writerow({'materia': i,
+                            fieldnames[1]:voti[0],
+                            fieldnames[2]:voti[1],
+                            fieldnames[3]:voti[2],
+                            fieldnames[4]:voti[3],
+                            fieldnames[5]:voti[4],
+                            fieldnames[6]:voti[5],
+                            fieldnames[7]:voti[6],
+                            fieldnames[8]:voti[7],
+                            fieldnames[9]:voti[8],
+                            fieldnames[10]:voti[9],
+                            fieldnames[11]:m
+                            })
+
+    csv_path = os.path.abspath('example.csv')
+    if platform.system() == 'Windows':
+        os.startfile(csv_path)
+    elif platform.system() == 'Darwin':
+        subprocess.call(['open', csv_path])
+    else:
+        subprocess.call(['xdg-open', csv_path])
+
---
+++ media_voti.py
@@ -0,0 +1,154 @@
+# media voti
+
+# librerie
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+import time
+import CSV_Voti
+import getpass
+
+# chek voti
+def chek():
+    scelta=input("vuoi cambiare user? (y/n): ").lower()
+    if scelta=="n":
+        codice="S11277444G"
+    else:
+        codice=input("codice: ")
+
+    password=getpass.getpass("password: ")
+    input(f"hai inserito una password lunga {len(password)} caratteri, invia per proseguire: ")
+    # usa in background
+    option = Options()
+    option.add_argument("--headless")
+
+    # setup driver
+    service = Service("chromedriver.exe")  
+    driver = webdriver.Chrome(service=service,options=option)
+
+    # applica stealth
+    stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win64",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+    )
+
+    # open the website
+    driver.get("https://web.spaggiari.eu/home/app/default/login.php")
+
+    # login
+    user=driver.find_element(By.ID, "login")
+    password_log=driver.find_element(By.ID, "password")
+    user.send_keys(codice)
+    password_log.send_keys(password)
+    bottone = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    bottone.click()
+
+    time.sleep(10)
+    try:
+        bottone = driver.find_element(By.CSS_SELECTOR, "button[type='button']")
+        bottone.click()
+    except:
+        pass
+    try:
+        anno_precedente = driver.find_element(By.CSS_SELECTOR, "p[class='handwriting_2 ']")
+        anno_precedente.click()
+    except:
+        pass
+    # Passa alla nuova finestra/scheda
+    try:
+        time.sleep(2)  # attesa breve per apertura finestra
+        windows = driver.window_handles
+        driver.switch_to.window(windows[-1])
+    except:
+        print("password errata")
+
+    time.sleep(10)
+    # Trova tutte le righe della tabella con classe 'griglia rigtab'
+    righe = driver.find_elements(By.CSS_SELECTOR, "tr[align='left']")
+    if len(righe) >= 5:
+        righe[4].click()  # Indici Python partono da 0, quindi la quinta è [4]
+    else:
+        print("Meno di 5 righe trovate con classe 'griglia rigtab' oppure hai messo le credenziali sbagliate")
+        return
+    time.sleep(4)
+    # Clicca sul bottone che contiene il testo "Tutto"
+    tutto_button = driver.find_element(By.XPATH, "//button[contains(., 'Tutto')]")
+    tutto_button.click()
+
+    time.sleep(8)
+
+    # Trova tutti i voti nei paragrafi all'interno della struttura tr>td>div>p
+    voti = []
+    trs = driver.find_elements(By.TAG_NAME, "tr")
+    for tr in trs:
+        tcs = tr.find_elements(By.TAG_NAME, "td")
+        for tc in tcs:
+            if tc.get_attribute("colspan") == "48":
+                voti.append(tr.text.strip())
+            if tc.get_attribute("colspan") == "2":
+                divs = tc.find_elements(By.TAG_NAME, "div")
+                for div in divs:
+                    if div.get_attribute("class").strip() == "cella_div cella_div f_reg_voto_dettaglio":
+                        continue
+                    paragrafi = div.find_elements(By.TAG_NAME, "p")
+                    for p in paragrafi:
+                        testo = p.text.strip()
+                        if testo:  # solo paragrafi non vuoti
+                            voti.append(testo)
+    voti=voti[3:]
+    print("Voti trovati:", voti)
+    with open("voti.txt", "w") as file:
+        for voto in voti:
+            file.write(voto + "\n")
+        file.write("riga adizzionale")
+    driver.quit()
+    print("dati aggiornati!")
+
+
+def media():
+    with open("voti.txt", "r") as file:
+        voti = file.readlines()
+        totale=0
+        for voto in voti:
+            voto = voto.strip()
+            if "-" in voto:
+                voto=voto.replace("-", "")
+                voto=float(voto)-0.25
+            elif "+" in voto:
+                voto=voto.replace("+", "")
+                voto=float(voto)+0.25
+            elif "½" in voto:
+                voto=voto.replace("½", "")
+                voto=float(voto)+0.5
+            else:
+                try:
+                    voto=float(voto)
+                except:
+                    continue
+            totale+=voto
+            m=totale/(len(voti)-12)
+        print("Media dei voti:", m)
+    
+def main():
+    print("""
+1) aggiorna lista voti
+2) media
+3) csv
+          """)
+    
+    scelta=int(input("Scegli un'opzione: "))
+    if scelta==1:
+        chek()
+    elif scelta==2:
+        media()
+    elif scelta==3:
+        CSV_Voti.csv()
+
+while True:
+    main()
